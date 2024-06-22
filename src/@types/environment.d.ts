ab1f738f5cd51b31019849998d825b9ee909b824
yago-codev <yagomilano92@gmail.com>
2024-06-22 19:35:02 -0300

    update env variables

--- src/@types/environment.d.ts
+++ src/@types/environment.d.ts
@@ -1,12 +1,12 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      REACT_APP_FIREBASE_API_KEY: string;
-      REACT_APP_FIREBASE_AUTH_DOMAIN: string;
-      REACT_APP_FIREBASE_PROJECT_ID: string;
-      REACT_APP_FIREBASE_STORAGE_BUCKET: string;
-      REACT_APP_FIREBASE_MESSAGING_SENDER_ID: string;
-      REACT_APP_FIREBASE_APP_ID: string;
+      VITE_APP_FIREBASE_API_KEY: string;
+      VITE_APP_FIREBASE_AUTH_DOMAIN: string;
+      VITE_APP_FIREBASE_PROJECT_ID: string;
+      VITE_APP_FIREBASE_STORAGE_BUCKET: string;
+      VITE_APP_FIREBASE_MESSAGING_SENDER_ID: string;
+      VITE_APP_FIREBASE_APP_ID: string;
     }
   }
 }
